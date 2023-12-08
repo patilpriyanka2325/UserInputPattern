377ecf3a3c035f7a3e14b20795b0bdc59fa45add
PRIYANKA <PRIYANKA@LAPTOP-GO8TVDUH>
2023-12-08 18:29:32 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Pattern2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number :");
+            var number = Convert.ToInt32(Console.ReadLine());
+            for (int i = number; i >= 1; i--)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("*");
+                    if (j > i)
+                    {
+                        for (int k = i; k <= j; k++)
+                        {
+                            Console.Write(" ");
+                        }
+                    }
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
